591e84bd501006e824cece2b373d3c0194988c23
Brune <168581255+BruneCoding@users.noreply.github.com>
2025-03-23 12:42:55 +0000

    Indexed DB Support

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
+console.log("Version 0.0.1");
 let rotation = 0;
 let isFrontVisible = true;
+let db;
+const request = indexedDB.open("ToggleDB", 1);
+request.onupgradeneeded = function (e) {
+  db = e.target.result;
+  db.createObjectStore("state", { keyPath: "id" });
+};
+request.onsuccess = function (e) {
+  db = e.target.result;
+  loadState();
+};
+request.onerror = function (e) {
+  console.error("DB Error:", e);
+};
+
+function saveState() {
+  const tx = db.transaction("state", "readwrite");
+  const store = tx.objectStore("state");
+  store.put({ id: 1, isFrontVisible, rotation });
+}
+
+function loadState() {
+  const tx = db.transaction("state", "readonly");
+  const store = tx.objectStore("state");
+  const getReq = store.get(1);
+  getReq.onsuccess = () => {
+    if (getReq.result) {
+      isFrontVisible = getReq.result.isFrontVisible;
+      rotation = getReq.result.rotation;
+      document.querySelector(
+        ".circle-container"
+      ).style.transform = `rotateY(${rotation}deg)`;
+    }
+  };
+}
 
 function rotateCircle() {
   rotation += 180;
@@ -7,9 +42,9 @@ function rotateCircle() {
   document.querySelector(
     ".circle-container"
   ).style.transform = `rotateY(${rotation}deg)`;
+  saveState();
   createSparkles();
 }
-
 function createSparkles() {
   const container = document.querySelector(".circle-container");
   for (let i = 0; i < 10; i++) {
